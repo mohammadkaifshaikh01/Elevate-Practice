83423c6fb5c42b05389fd824667af9f476c5c652
Mohammad Kaif Shaikh <mohammadkaif6268@gmail.com>
2025-05-03 15:13:26 +0530

    Something changes

--- React/RiteshSirClass/src/App.jsx
+++ React/RiteshSirClass/src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import Home from './pages/Home'
 import { Routes, Route } from 'react-router-dom';
 import CharacterDetails from './pages/CharacterDetails';
+import ThemeToggle from './Components/ThemeToggle';
 
 const App = () => {
   return (
     <div>
+    <ThemeToggle />
     <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/character/:id" element={<CharacterDetails />} />
---
+++ React/RiteshSirClass/src/Components/ThemeToggle.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ThemeToggle = () => {
+  return (
+    <div>
+       <button
+      // onClick={toggleTheme}
+      className="p-2 rounded bg-gray-200 dark:bg-gray-700 text-black dark:text-white"
+    >
+      Toggle to Mode
+    </button>
+    </div>
+  )
+}
+
+export default ThemeToggle
