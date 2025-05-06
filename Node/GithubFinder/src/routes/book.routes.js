8715bdeb3874212ae7ba23e3abe8ad0e1edb298c
Mohammad Kaif Shaikh <mohammadkaif6268@gmail.com>
2025-05-06 12:30:57 +0530

    Post , Read , Delete , Update All apis working properly backend done

--- Node/GithubFinder/src/routes/book.routes.js
+++ Node/GithubFinder/src/routes/book.routes.js
@@ -1,13 +1,14 @@
 import express from "express"
-import {addBook ,getBook} from "../controller/library.controller.js"
+import {addBook ,deleteBook,getBook ,updateBook} from "../controller/library.controller.js"
 
 
 const bookRoute = express.Router()
 
 
 bookRoute.post("/add-book" , addBook)
-// bookRoute.get("/get-book" , getBook)
 bookRoute.get("/get-book" , getBook)
+bookRoute.patch("/update-book/:id" , updateBook)
+bookRoute.delete("/delete-book/:id" , deleteBook)
 
 
 export default  bookRoute
